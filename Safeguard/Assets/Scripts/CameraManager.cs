6a8513cc5f5b0d5778968275e6dc2e6ecaa10a73
unknown <nolan.r@husky.neu.edu>
2018-03-18 13:26:14 -0400

    Camera Stuff

---
+++ Safeguard/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	void Start () {
+		GameObject gm = GameObject.Find ("Main Camera");
+		Camera c = gm.GetComponent<Camera> ();
+		GameObject dm = GameObject.Find ("GameManager");
+		int length = dm.GetComponent<GridManager> ().gridSideLength;
+		c.transform.position = new Vector3 (dm.GetComponent<GridManager> ().getGrid () [((length * 2) - 1) * (length - 1) + length - 1].GetComponent<Transform> ().position.x, 
+			dm.GetComponent<GridManager> ().getGrid () [((length * 2) - 1) * (length - 1) + length].GetComponent<Transform> ().position.y, -10);
+		c.orthographicSize = 16 + (length - 4) * 5;
+	}
+
+}
