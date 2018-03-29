7483c1b198b220920130a87c94dc4b7c68c0b31e
unknown <nolan.r@husky.neu.edu>
2018-03-28 21:46:38 -0400

    Added PopupText prefab

---
+++ Safeguard/Assets/Scripts/HoverTextManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverTextManager : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().sortingOrder = 11;
+		GetComponent<Renderer> ().sortingLayerName = "Player";
+		GetComponentInChildren<SpriteRenderer> ().sortingLayerName = "Player";
+		GetComponentInChildren<SpriteRenderer> ().sortingOrder = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (HexManager.textStatus == false) {
+			Destroy (gameObject);
+		}
+	}
+}
+ 
